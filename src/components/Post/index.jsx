5ce6522a21fcc7aca98f8e6c6e1e1c134e62fdb4
Neto Russo <netoacrn1@gmail.com>
2025-03-23 01:58:55 -0300

    Aula 12

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,15 +1,11 @@
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className="post mx-2">
       <div className="img-post"></div>
-      <h5>Tecnologia</h5>
-      <h3>O guia definitvo do Blog</h3>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore laborum,
-        llum fugit doloribus optio quas, voluptate tenetur saepe
-        ipsa quod error debitis aliquid nulla facere omnis
-        exercitationem quibusdam magni. Cum.
-      </p>
+      <h5>{props.subtitle}</h5>
+      <h3>{props.title}</h3>
+      <p className="mt-1">{props.children}</p>
       <div className="flex pt-2">
         <div className="img-profile "></div>
         <div className="description-profile ml-2">
