090238659f8698f19719441d67f4565c2b0e2c0a
Neto Russo <netoacrn1@gmail.com>
2025-03-23 00:48:42 -0300

    Aula 10

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,26 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import logosvg from "../../svg/logo.svg";
 
 const Header = () => {
+
+  const descLogo = "Essa é a logo do blog"
+
   return (
-    <h1>Header.js</h1>
+    <>
+      <header className="flex-space-between">
+        <div className="logo">
+          <img src={logosvg} alt={descLogo} />
+        </div>
+        <div className="search">
+          <input className="input-search" type="text" name="search"  />
+        </div>
+        <ul className="menu">
+          <li><a href="#" className="nav-link">Categories</a></li>
+          <li><a href="#" className="nav-link">About</a></li>
+          <li><a href="#" className="nav-link">Contact</a></li>
+        </ul>
+      </header>
+    </>
   )
 }
 
