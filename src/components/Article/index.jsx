4b3c6d6ff749d5c840675d92211f0194518d9030
Neto Russo <netoacrn1@gmail.com>
2025-03-23 01:41:05 -0300

    Aula 11

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -4,10 +4,16 @@ import Post from "../Post";
 const Article = () => { 
 
   return (
-    <>
+    <section className="container">
       <h2>Article.jsx</h2>
+
+      <div className="container-posts mt-5">
       <Post />
-    </>
+      <Post />
+      <Post />
+
+      </div>
+    </section>
   )
 }
 
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,7 +1,23 @@
 
 const Post = () => {
   return (
-    <h3>Post.jsx</h3>
+    <div className="post mx-2">
+      <div className="img-post"></div>
+      <h5>Tecnologia</h5>
+      <h3>O guia definitvo do Blog</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore laborum,
+        llum fugit doloribus optio quas, voluptate tenetur saepe
+        ipsa quod error debitis aliquid nulla facere omnis
+        exercitationem quibusdam magni. Cum.
+      </p>
+      <div className="flex pt-2">
+        <div className="img-profile "></div>
+        <div className="description-profile ml-2">
+          <h6 className="color-blue">Fulano de tal</h6>
+          <p>Mac, 23, 2025 - 8 min read</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
